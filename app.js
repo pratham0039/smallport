3783610ff55812c842024a966aecb546cd004836
pratham0039 <98754411+pratham0039@users.noreply.github.com>
2023-12-17 13:43:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const canvas = document.querySelector(".canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d");
+
+const frameCount = 200;
+
+const currentFrame = (index) => `./anime/${(10000+index + 1).toString()}.jpg`;
+
+const images = [];
+
+let ball = { frame: 0 };
+for(let i = 0; i<frameCount;i++){
+    const img = new Image();
+    img.src = currentFrame(i);
+    images.push(img);
+}
+
+gsap.to(ball, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: "none",
+    scrollTrigger: {
+      scrub: 0.5,
+      pin: "canvas",
+      end: "500%",
+    },
+    onUpdate: render,
+  });
+
+images[0].onload = render;
+
+function render() {
+    context.canvas.width = images[0].width;
+    context.canvas.height = images[0].height;
+  
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(images[ball.frame], 0, 0);
+  }
